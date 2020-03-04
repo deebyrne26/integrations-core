51832a1f8f7684bb2d45e3f546e046c2c2ffb544
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-03-04 17:31:24 +0100

    Update e2e

--- confluent_platform/tests/test_e2e.py
+++ confluent_platform/tests/test_e2e.py
@@ -11,7 +11,37 @@ from .metrics import build_metrics
 # https://docs.confluent.io/current/kafka/monitoring.html#broker-metrics
 
 BROKER_METRICS = [
+    'kafka.cluster.partition.under_min_isr',
+    'kafka.connect.connect_worker_metrics.connector_count',
+    'kafka.connect.connect_worker_metrics.connector_startup_attempts_total',
+    'kafka.connect.connect_worker_metrics.connector_startup_failure_percentage',
+    'kafka.connect.connect_worker_metrics.connector_startup_failure_total',
+    'kafka.connect.connect_worker_metrics.connector_startup_success_percentage',
+    'kafka.connect.connect_worker_metrics.connector_startup_success_total',
+    'kafka.connect.connect_worker_metrics.task_count',
+    'kafka.connect.connect_worker_metrics.task_startup_attempts_total',
+    'kafka.connect.connect_worker_metrics.task_startup_failure_percentage',
+    'kafka.connect.connect_worker_metrics.task_startup_failure_total',
+    'kafka.connect.connect_worker_metrics.task_startup_success_percentage',
+    'kafka.connect.connect_worker_metrics.task_startup_success_total',
+    'kafka.controller.controller_stats.leader_election_rate_and_time_ms.avg',
+    'kafka.controller.kafka_controller.active_controller_count',
+    'kafka.controller.kafka_controller.offline_partitions_count',
+    'kafka.network.request_channel.request_queue_size',
+    'kafka.network.request_metrics.local_time_ms.avg',
+    'kafka.network.request_metrics.remote_time_ms.avg',
+    'kafka.network.request_metrics.request_queue_time_ms.avg',
+    'kafka.network.request_metrics.response_queue_time_ms.avg',
+    'kafka.network.request_metrics.response_send_time_ms.avg',
+    'kafka.network.request_metrics.total_time_ms.avg',
+    'kafka.network.socket_server.network_processor_avg_idle_percent',
+    'kafka.server.delayed_operation_purgatory.purgatory_size',
+    'kafka.server.delayed_operation_purgatory.purgatory_size',
+    'kafka.server.replica_fetcher_manager.max_lag',
+    'kafka.server.replica_manager.leader_count',
+    'kafka.server.replica_manager.partition_count',
     'kafka.server.replica_manager.under_min_isr_partition_count',
+    'kafka.server.replica_manager.under_replicated_partitions',
 ]
 
 ALL_METRICS = BROKER_METRICS
@@ -27,10 +57,8 @@ def test_e2e(dd_agent_check):
         if metric_name.startswith('jvm.'):
             aggregator.assert_metric(metric_name)
 
-    for metric in build_metrics(checked_only=True):
-        metric_name = metric['metric_name']
-        metric_type = getattr(aggregator, metric['metric_type'].upper())
-        aggregator.assert_metric(metric_name, metric_type=metric_type)
+    for metric in ALL_METRICS:
+        aggregator.assert_metric(metric)
 
     # aggregator.assert_all_metrics_covered()
 
